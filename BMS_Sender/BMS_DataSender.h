bd33fd8f2f286bd41583f05a02e6f4f216e322eb
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-01 20:43:42 +0530

    Update BMS_DataSender.h

--- BMS_Sender/BMS_DataSender.h
+++ BMS_Sender/BMS_DataSender.h
@@ -1,1 +1,25 @@
+/******************************************************************************
+*****     (C) COPYRIGHT Robert Bosch GmbH CM-IS - All Rights Reserved     *****
+******************************************************************************/
+/*!
+***     \file        BMS_DataSender.h
+***
+***     \author      Abinesh KanjiKovil Thatchinamoorthy
+***
+\par    File_description
+***                  Header for the BMS data streaming source file
+*\n*/
+/*********************************************************************************/
 
+
+/* MACROS */
+
+
+/* Enums */
+typedef enum
+{
+	STATUS_FAILED,
+	STARUS_SUCCESSFUL
+}Bms_DataTransSts;
+
+Bms_DataTransSts readSensorData_e(void);
