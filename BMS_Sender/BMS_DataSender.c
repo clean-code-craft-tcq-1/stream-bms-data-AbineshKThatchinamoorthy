56d37c75d806b7f8cc070de3860913708fbe37aa
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-01 20:43:43 +0530

    Update BMS_DataSender.c

--- BMS_Sender/BMS_DataSender.c
+++ BMS_Sender/BMS_DataSender.c
@@ -4,9 +4,31 @@
 /*!
 ***     \file        BMS_DataSender.c
 ***
-***     \author      Abinesh.KanjiKovilThatchinamoorthy@in.bosch.com
+***     \author      Abinesh KanjiKovil Thatchinamoorthy
 ***
 \par    File_description
-***                  This modules provides the Service Key 1a functionality
+***                  Implementation of BMS data streaming to console
 *\n*/
 /*********************************************************************************/
+
+
+/* INCLUSIONS */
+#include <stdio.h>
+
+Bms_DataTransSts readSensorData_e()
+{
+  Bms_DataTransSts readSts_e;
+  FILE * SensorIpFile = fopen("./BMS_Sender/SensorData.txt","r");  
+  
+  if (NULL != SensorIpFile) {
+        for(int cnt = 0;fscanf(file, "%f\t\t%f\n", &TemperatureTemp,&StateOfChargeVal)!=EOF ;cnt++)
+        {
+
+        }		
+	}
+  else {
+    printf("File open attempt failed\n");
+  }
+  
+  return readSts_e;
+}
