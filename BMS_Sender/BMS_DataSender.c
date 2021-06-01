c383d0c308185ec4f73971488976cde25dac94e4
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-01 18:28:20 +0530

    Update and rename BMS_Sender.c to BMS_DataSender.c

---
+++ BMS_Sender/BMS_DataSender.c
@@ -0,0 +1,12 @@
+/******************************************************************************
+*****     (C) COPYRIGHT Robert Bosch GmbH CM-IS - All Rights Reserved     *****
+******************************************************************************/
+/*!
+***     \file        BMS_DataSender.c
+***
+***     \author      Abinesh.KanjiKovilThatchinamoorthy@in.bosch.com
+***
+\par    File_description
+***                  This modules provides the Service Key 1a functionality
+*\n*/
+/*********************************************************************************/
