996c819ba84b35c0111fcbdfd6f5874cf6bf7e52
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-06-02 18:32:46 +0530

    Update BMS_DataSender.cpp

--- BMS_Sender/BMS_DataSender.cpp
+++ BMS_Sender/BMS_DataSender.cpp
@@ -6,7 +6,12 @@
 
 TEST_CASE("validate the Read operation function - readFileData_e") {
 	
-	StreamOpSts_t streamOpSts_s = {0,0,0};
+	StreamOpSts_t streamOpSts_s;
+	
+	streamOpSts_s.readSts_e   = OP_MODE_STATUS_INIT;
+        streamOpSts_s.streamSts_e = OP_MODE_STATUS_INIT;
+	streamOpSts_s.streamedDataLen_i = 0;
+	
 	streamOpSts_s = readFileData_e();
 	
   	REQUIRE(streamOpSts_s.readSts_e   == OP_MODE_STATUS_SUCCESSFUL);
@@ -16,7 +21,12 @@ TEST_CASE("validate the Read operation function - readFileData_e") {
 
 TEST_CASE("validate the case that the User has not requested the Stream operation ") {
 	
-	StreamOpSts_t streamOpSts_s = {0,0,0};
+	StreamOpSts_t streamOpSts_s;
+	
+	streamOpSts_s.readSts_e   = OP_MODE_STATUS_INIT;
+        streamOpSts_s.streamSts_e = OP_MODE_STATUS_INIT;
+	streamOpSts_s.streamedDataLen_i = 0;
+	
 	streamOpSts_s = initiateStream_v(0);
 	
   	REQUIRE(streamOpSts_s.readSts_e   == OP_MODE_STATUS_INIT);
@@ -25,7 +35,12 @@ TEST_CASE("validate the case that the User has not requested the Stream operatio
 
 TEST_CASE("validate the case that the User has requested the Stream operation ") {
 	
-	StreamOpSts_t streamOpSts_s = {0,0,0};
+	StreamOpSts_t streamOpSts_s;
+	
+	streamOpSts_s.readSts_e   = OP_MODE_STATUS_INIT;
+        streamOpSts_s.streamSts_e = OP_MODE_STATUS_INIT;
+	streamOpSts_s.streamedDataLen_i = 0;
+	
 	streamOpSts_s = initiateStream_v(1);
 	
   	REQUIRE(streamOpSts_s.readSts_e   == OP_MODE_STATUS_SUCCESSFUL);
