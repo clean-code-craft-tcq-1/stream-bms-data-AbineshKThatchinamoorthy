7cf132f7b81671e1d9645812cfa039ec58037242
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-06-21 18:36:39 +0530

    Update BMS_receiver.h

--- receiver/BMS_receiver.h
+++ receiver/BMS_receiver.h
@@ -1,2 +1,25 @@
-#pragma once
- int receiver();
+#ifndef BMS_RECEIVER_H
+#define BMS_RECEIVER_H
+
+/******************************************************************************
+*****     (C) COPYRIGHT Robert Bosch GmbH CM-IS - All Rights Reserved     *****
+******************************************************************************/
+/*!
+***     \file        BMS_receiver.h
+***
+***     \author      Pratik Jaiswal
+***
+\par    File_description
+***                  Header for the BMS data receiving source file
+*\n*/
+/*********************************************************************************/
+
+/* ******************************Inclusions************************************ */
+#include <stdio.h>
+
+/* ******************************function definition************************************ */
+
+void ReadData(char *DatafromConsole);
+
+
+#endif
