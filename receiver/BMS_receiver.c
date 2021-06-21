45ef3f0a411ba5c2079e7cffe407ceec1c0798d3
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-06-21 22:25:19 +0530

    Update BMS_receiver.c

--- receiver/BMS_receiver.c
+++ receiver/BMS_receiver.c
@@ -1,20 +1,39 @@
 #include "BMS_receiver.h"
 #include <stdio.h>
 
+static int array_temp[10];
+static int array_soc[10];
+static char data[500];
 
-void ReadData(char *DatafromConsole)
+void ReadData()
 {
         if(DatafromConsole!=NULL)
         {
-                scanf("%s",DatafromConsole);
+               // scanf("%s",DatafromConsole);
                 printf("Hello/n");
                       
         }
+        while (fgets(data, 500, stdin))
+   {
+	char * flag = strtok(data, "/n");
+   	while( flag != NULL ) 
+	{
+		if(isdigit(*flag))
+		{
+			if(i<10)	
+				array_temp[i] = atoi(flag);
+			else
+				array_soc[i] = atoi(flag);
+			i++;
+		}	
+      		flag = strtok(NULL, "/n");
+   	}
+    }
 }
                       
  int main()
  {
          char *s = "hello receiver";
-         ReadData(s);
+         ReadData();
          return 0;
  }
