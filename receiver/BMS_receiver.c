40e3c51d2328f7169a87c05ce06b4ec17f260e49
Pratik Jaiswal <66913702+AnPra7@users.noreply.github.com>
2021-06-22 10:41:42 +0530

    Update BMS_receiver.c

--- receiver/BMS_receiver.c
+++ receiver/BMS_receiver.c
@@ -1,24 +1,27 @@
+/******************************************************************************
+*****     (C) COPYRIGHT Robert Bosch GmbH CM-IS - All Rights Reserved     *****
+******************************************************************************/
+/*!
+***     \file        BMS_receiver.h
+***
+***     \author      Pratik Jaiswal
+***
+\par    File_description
+***                  Header for the BMS data receiving source file
+*\n*/
+/*********************************************************************************/
+
+/* ******************************Inclusions************************************ */
+
 #include "BMS_receiver.h"
 #include <stdio.h>
 
-#define datalen 50
-
-#define TEMPMAX  45.f
-#define TEMPMIN  0.f
-
-#define SOCMAX  0.8f
-#define SOCMIN  0.f
-
-typedef enum Entity
-{
-  Temperature,
-  SOC
-}Entity;
-
-void GetValue(char *_getvalue, float *Value_);
-void FindMinVal(float *Val_rx, float *MinVal);
-void FindMaxVal(float *Val_rx, float *MaxVal);
-void _ReadReceiver();
+/* Function Details *******************************************************************************************
+* Function Name : ReadData
+* Description   : Reads the Data from the the sender
+* Arguments     : DatafromConsole(char)
+* Returns       : bool if the reading is successfull and the character is not NULL
+* *********************************************************************************************************** */
 
 bool ReadData(char *DatafromConsole)
 {
@@ -36,7 +39,12 @@ bool ReadData(char *DatafromConsole)
         }
         return false;
    }
-
+/* Function Details *******************************************************************************************
+* Function Name : GetValue
+* Description   : receives value from console and stores it
+* Arguments     : (char) _getvalue, (float) Value_
+* Returns       : void
+* *********************************************************************************************************** */
 void GetValue(char *_getvalue, float *Value_)
 {
   int i = 0, j = 0; 
@@ -52,7 +60,12 @@ void GetValue(char *_getvalue, float *Value_)
     printf("Paramter value = %f\n", Value_[parNum]); 
 
 }
-
+/* Function Details *******************************************************************************************
+* Function Name : _ReadReceiver
+* Description   : reads the value and stores the maximum,min value
+* Arguments     : -
+* Returns       : -
+* *********************************************************************************************************** */
 void _ReadReceiver()
 {
   char DatafromConsole[datalen];
@@ -81,7 +94,12 @@ void _ReadReceiver()
   }
   
 }
-
+/* Function Details *******************************************************************************************
+* Function Name : FindMinVal
+* Description   : Checks the current value with the Previous min value
+* Arguments     : Val_rx(float), MinVal(float)
+* Returns       : current_value(float) or prev_maxi(float) which is greater
+* *********************************************************************************************************** */
 void FindMinVal(float *Val_rx, float *MinVal) {
    
    
@@ -91,7 +109,12 @@ void FindMinVal(float *Val_rx, float *MinVal) {
 
     }
 }
-
+/* Function Details *******************************************************************************************
+* Function Name : FindMaxVal
+* Description   : Checks the current value with the Previous ax value
+* Arguments     : Val_rx(float), MaxVal(float)
+* Returns       : Val_rx(float) or prev_maxi(float) which is greater
+* *********************************************************************************************************** */
 void FindMaxVal(float *Val_rx, float *MaxVal) {
  
   if (*MaxVal < *Val_rx)
